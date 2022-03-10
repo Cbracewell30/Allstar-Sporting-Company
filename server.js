4e28c618649e958e2fab820dec5de30a43f831c0
Jimmy Rando <jsturat46925@gmail.com>
2022-03-09 21:11:21 -0500

    add basic format for routes and sequelize is running maybe

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-// const routes = require("./routes");
+const routes = require("./routes");
+const sequelize = require('./config/connection');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -7,8 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('App is now listening!'));
 });
