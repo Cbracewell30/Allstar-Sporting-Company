b6a7e260ab9ee8dc7af069994c3401fb821450a0
Chris <chrisbracewell35@gmail.com>
2022-03-14 00:20:28 -0400

    adding user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,26 +1,92 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
 //add models request here
-
+const { User, Store } = require("../../models");
 
 //add route to get all, model.findAll
-router.get('/', (req, res) => { });
-
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //add route to get 1, model.findOne
-router.get('/:id', (req, res) => { });
-
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Store,
+        attributes: ["store_name", "store_location"],
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //add route to create, model.create
-router.post('/', (req, res) => { });
+router.post("/", (req, res) => {
+  User.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    store_id: req.body.store_id,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //add route to update 1, model.update
-router.put('/:id', (req, res) => { });
-
+router.put("/:id", (req, res) => {});
 
 //add route to delete 1, model.destroy
-router.delete('/', (req, res) => { });
-
+router.delete("/", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
