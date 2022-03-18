40ac3798151b553e82bbf4d968fcd770c9712b49
Allison Demkovich <demkovich.allison@gmail.com>
2022-03-17 23:03:26 -0500

    get add product to add to db

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -6,6 +6,7 @@ const { Product, User, Rate, Store } = require("../../models");
 
 //add route to get all products, model.findAll
 router.get("/", (req, res) => {
+  res.render('new-product');
   Product.findAll({
     attributes: ['id', 'name', 'price', 'stock', 'store_id', 'filename', 'description']
   })
@@ -21,38 +22,40 @@ router.get("/", (req, res) => {
 
 //add route to get 1 product, model.findOne
 router.get("/:id", (req, res) => {
-  res.render('new-product');
-  // Product.findOne({
-  //   where: {
-  //     id: req.params.id,
-  //   },
-  //     attributes: ['id', 'name', 'price', 'stock', 'store_id', 'filename', 'description']
-  // })
-  //   .then((dbProductData) => {
-  //     //display message if id value has no product
-  //     if (!dbProductData) {
-  //       res.status(404).json({ message: "No product has this id." });
-  //       return;
-  //     }
-  //     res.json(dbProductData);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(500).json(err);
-  //   });
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+      attributes: ['id', 'name', 'price', 'stock', 'store_id', 'filename', 'description']
+  })
+    .then((dbProductData) => {
+      //display message if id value has no product
+      if (!dbProductData) {
+        res.status(404).json({ message: "No product has this id." });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 //add route to create new product, model.create
-router.post("/", withAuth, (req, res) => {
-  res.render('new-product');
+router.post("/", (req, res) => {
   Product.create({
     name: req.body.name,
     price: req.body.price,
     stock: req.body.stock,
     store_id: req.body.store_id,
-    rating: req.body.rating,
+    filename: req.body.filename,
+    description: req.body.description
   })
-    .then((dbProductData) => res.json(dbProductData))
+    .then((dbProductData) => {
+      res.json(dbProductData);
+
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -69,7 +72,7 @@ router.put("/:id", (req, res) => {
     .then((dbProductData) => {
       //display message if id value has no product
       if (!dbProductData) {
-        res.status(404).json({ message: "No product has this id." });
+        res.status(404).json({ message: "No" });
         return;
       }
       res.json(dbProductData);
@@ -102,76 +105,76 @@ router.delete("/:id", (req, res) => {
 });
 
 
-router.get('/', withAuth, (req, res) => {
-  Product.findAll({
-          where: {
-              user_id: req.session.user_id
-          },
-          attributes: [
-              'name',
-              'price',
-              'stock',
-              'store_id',
-              'filename',
-              'description'
-          ],
-          include: [{
-                  model: Product,
-                  attributes: ['id', 'name', 'price', 'stock', 'store_id', 'filename', 'description'],
-                  include: {
-                      model: User,
-                      attributes: ['email']
-                  }
-              },
-              {
-                  model: User,
-                  attributes: ['email']
-              }
-          ]
-      })
-      .then(dbPostData => {
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-          res.render('product', { posts, loggedIn: true });
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
-});
+// router.get('/', withAuth, (req, res) => {
+//   Product.findAll({
+//           where: {
+//               user_id: req.session.user_id
+//           },
+//           attributes: [
+//               'name',
+//               'price',
+//               'stock',
+//               'store_id',
+//               'filename',
+//               'description'
+//           ],
+//           include: [{
+//                   model: Product,
+//                   attributes: ['id', 'name', 'price', 'stock', 'store_id', 'filename', 'description'],
+//                   include: {
+//                       model: User,
+//                       attributes: ['email']
+//                   }
+//               },
+//               {
+//                   model: User,
+//                   attributes: ['email']
+//               }
+//           ]
+//       })
+//       .then(dbPostData => {
+//           const posts = dbPostData.map(post => post.get({ plain: true }));
+//           res.render('product', { posts, loggedIn: true });
+//       })
+//       .catch(err => {
+//           console.log(err);
+//           res.status(500).json(err);
+//       });
+// });
 
-// //edit product
-router.get('/edit/:id', withAuth, (req, res) => {
-  Product.findOne({
-          where: {
-              id: req.params.id
-          },
-          attributes: ['id',
-              'name',
-              'price',
-              'stock',
-              'store_id',
-              'filename',
-              'description'
-          ],
-      })
-      .then(dbProductData => {
-          if (!dbProductData) {
-              res.status(404).json({ message: 'No product found with this id' });
-              return;
-          }
+// // //edit product
+// router.get('/edit/:id', withAuth, (req, res) => {
+//   Product.findOne({
+//           where: {
+//               id: req.params.id
+//           },
+//           attributes: ['id',
+//               'name',
+//               'price',
+//               'stock',
+//               'store_id',
+//               'filename',
+//               'description'
+//           ],
+//       })
+//       .then(dbProductData => {
+//           if (!dbProductData) {
+//               res.status(404).json({ message: 'No product found with this id' });
+//               return;
+//           }
 
-          const post = dbProductData.get({ plain: true });
-          res.render('edit-product', { post, loggedIn: true });
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
-});
+//           const post = dbProductData.get({ plain: true });
+//           res.render('edit-product', { post, loggedIn: true });
+//       })
+//       .catch(err => {
+//           console.log(err);
+//           res.status(500).json(err);
+//       });
+// });
 
 
-router.get('/new', (req, res) => {
-  res.render('new-product');
-});
+// router.get('/new', (req, res) => {
+//   res.render('new-product');
+// });
 
 module.exports = router;
--- public/js/add-products.js
+++ public/js/add-products.js
@@ -1,14 +1,14 @@
 async function newFormHandler(event) {
     event.preventDefault();
   
-    const name = document.querySelector('input[name="products-name"]').value;
+    const name = document.querySelector('#products-name').value.trim();
     const price = document.querySelector('input[name="price"]').value;
     const stock = document.querySelector('input[name="stock"]').value;
-    const storeId = document.querySelector('input[name="store_id"]').value;
-    // const filename = document.querySelector('input[name="filename"]').value;
-    // const description = document.querySelector('input[name="description"]').value;
+    const store_id = document.querySelector('input[name="store_id"]').value;
+    const filename = document.querySelector('input[name="filename"]').value;
+    const description = document.querySelector('input[name="description"]').value;
   
-    console.log(name, price, stock, storeId);
+    console.log(name, price, stock, store_id);
 
     const response = await fetch(`/api/products/`, {
       method: 'POST',
@@ -16,16 +16,18 @@ async function newFormHandler(event) {
         name,
         price,
         stock,
-        storeId
+        store_id,
+        filename,
+        description
       }),
       headers: {
         'Content-Type': 'application/json'
       }
     });
   
-    if (response.ok) {
-      document.location.replace('/api/products');
-    } else {
+    if (!response.ok) {
+    //   document.location.replace('/api/products');
+    // } else {
       alert(response.statusText);
     }
   };
