8983334672856a61205b34ff3804301e1666276d
Allison Demkovich <demkovich.allison@gmail.com>
2022-03-20 21:30:32 -0500

    fix new product script

--- public/js/add-products.js
+++ public/js/add-products.js
@@ -2,12 +2,10 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   //create references to product info on handlebars
-  const name = document.querySelector("#products-name").value.trim();
+  const name = document.querySelector("#products-name").value;
   const price = document.querySelector('input[name="price"]').value;
   const stock = document.querySelector('input[name="stock"]').value;
   const store_id = document.querySelector('input[name="store_id"]').value;
-  const filename = document.querySelector('input[name="filename"]').value;
-  const description = document.querySelector('input[name="description"]').value;
 
   //add new product using submitted data
   const response = await fetch(`/api/products/`, {
@@ -17,8 +15,6 @@ async function newFormHandler(event) {
       price,
       stock,
       store_id,
-      filename,
-      description,
     }),
     headers: {
       "Content-Type": "application/json",
